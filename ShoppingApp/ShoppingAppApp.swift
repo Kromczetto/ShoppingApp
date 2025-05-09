2871f0367345bb3dbcbe803b1b8de813b59ee335
Kromczetto <jakubkromolowski@gmail.com>
2025-05-09 19:30:26 +0200

    Initial Commit

---
+++ ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by Kuba Kromomołowski on 09/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
